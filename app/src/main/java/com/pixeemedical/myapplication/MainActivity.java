db31d404d648a2087db378fa7286f73e0a9085c6
adupin <alan.dupin@pixee-medical.com>
2018-07-10 12:34:16 +0200

    T46.3 test alan 3

--- app/src/main/java/com/pixeemedical/myapplication/MainActivity.java
+++ app/src/main/java/com/pixeemedical/myapplication/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i(TAG, "onCreate: test alan 2 ");
+        Log.i(TAG, "onCreate: test alan 3 ");
     }
 }
