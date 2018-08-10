6ef27471a112d5308e29f001cd75cc163559a785
adupin <alan.dupin@pixee-medical.com>
2018-08-10 09:43:40 +0200

    test

--- app/src/main/java/com/pixeemedical/myapplication/MainActivity.java
+++ app/src/main/java/com/pixeemedical/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int toto = 2;
+        int toto = 5;
 
 
 
